02919acc48f6f441aae5c1af32cb75eac29cb729
nocodeleaks <codeleaks@proton.me>
2023-04-21 15:30:48 -0300

    Update install.sh

--- helpers/install.sh
+++ helpers/install.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-# tested on fresh ubuntu 20.04
+echo Installation tTested on fresh Ubuntu (20.04|22.04) (ARM64|AMD64)
 
-echo INSTALL GCC
+echo Installing GCC
 apt install gcc -y
 
-echo INSTALL GO
-wget https://go.dev/dl/go1.20.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.linux-amd64.tar.gz
+echo Installing GO language
+
+# for rpm versions rpm --eval '%{_arch}'
+ARCH=`dpkg --print-architecture`
+
+echo Installing for arch ${ARCH}
+wget https://go.dev/dl/go1.20.3.linux-${ARCH}.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.3.linux-${ARCH}.tar.gz
 GOPATH=/usr/local/go
 PATH=$PATH:$GOPATH/bin
 ln -sf ${GOPATH}/bin/go /usr/sbin/go
@@ -14,23 +19,23 @@ sed -nir '/^export GOPATH=/!p;$a export GOPATH='${GOPATH} ~/.bashrc
 sed -nir '/^export PATH=/!p;$a export PATH='$PATH:$GOPATH/bin ~/.bashrc
 go version
 
-echo UPDATING QUEPASA LINK
+echo Updating Quepasa link
 ln -sf /opt/quepasa-source/src /opt/quepasa 
 
-echo UPDATING LOGGING
+echo Updating logging
 ln -sf /opt/quepasa-source/helpers/syslog.conf /etc/rsyslog.d/10-quepasa.conf
 
-echo UPDATING LOGROTATE
+echo Updating log rotate
 ln -sf /opt/quepasa-source/helpers/quepasa.logrotate.d /etc/logrotate.d/quepasa
 
 /bin/mkdir -p /var/log/quepasa
 /bin/chmod 755 /var/log/quepasa
 /bin/chown syslog:adm /var/log/quepasa
 
-echo RESTARTING SERVICES
+echo Restarting services
 systemctl restart rsyslog
 
-echo UPDATING SYSTEMD SERVICE
+echo Updating systemd service
 ln -sf /opt/quepasa-source/helpers/quepasa.service /etc/systemd/system/quepasa.service
 systemctl daemon-reload
 
