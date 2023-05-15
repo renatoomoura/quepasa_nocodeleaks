ac98441863cb8d358f1b1d1b83115137a9461607
nocodeleaks <codeleaks@proton.me>
2023-05-15 18:37:33 -0300

    form received view handler

--- src/controllers/form_receive_controller.go
+++ src/controllers/form_receive_controller.go
@@ -4,12 +4,11 @@ import (
 	"html/template"
 	"net/http"
 
-	. "github.com/nocodeleaks/quepasa/metrics"
 	models "github.com/nocodeleaks/quepasa/models"
 	whatsapp "github.com/nocodeleaks/quepasa/whatsapp"
 )
 
-// FormReceiveController renders route GET "/bot/{token}/receive"
+// FormReceiveController renders route GET "/form/server/{token}/receive"
 func FormReceiveController(w http.ResponseWriter, r *http.Request) {
 	data := models.QPFormReceiveData{PageTitle: "Receive", FormAccountEndpoint: FormAccountEndpoint}
 
@@ -20,28 +19,25 @@ func FormReceiveController(w http.ResponseWriter, r *http.Request) {
 		data.Number = server.GetWid()
 		data.Token = server.Token
 		data.DownloadPrefix = GetDownloadPrefix(server.Token)
+
+		// Evitando tentativa de download de anexos sem o bot estar devidamente sincronizado
+		status := server.GetStatus()
+		if status != whatsapp.Ready {
+			RespondNotReady(w, &ApiServerNotReadyException{Wid: server.GetWid(), Status: status})
+			return
+		}
+
+		queryValues := r.URL.Query()
+		paramTimestamp := queryValues.Get("timestamp")
+		timestamp, err := GetTimestamp(paramTimestamp)
+		if err != nil {
+			RespondServerError(server, w, err)
+			return
+		}
+
+		messages := GetMessages(server, timestamp)
+		data.Messages = messages
 	}
-
-	// Evitando tentativa de download de anexos sem o bot estar devidamente sincronizado
-	status := server.GetStatus()
-	if status != whatsapp.Ready {
-		RespondNotReady(w, &ApiServerNotReadyException{Wid: server.GetWid(), Status: status})
-		return
-	}
-
-	queryValues := r.URL.Query()
-	paramTimestamp := queryValues.Get("timestamp")
-	timestamp, err := GetTimestamp(paramTimestamp)
-	if err != nil {
-		MessageReceiveErrors.Inc()
-		RespondServerError(server, w, err)
-		return
-	}
-
-	messages := GetMessages(server, timestamp)
-	data.Messages = messages
-
-	MessagesReceived.Add(float64(len(messages)))
 
 	templates := template.Must(template.ParseFiles(
 		"views/layouts/main.tmpl",
